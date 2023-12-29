e365dbce2819096ed7099091e5c776debedc6750
luyandohambala <luyandohambala.1@gmail.com>
2023-12-29 11:58:56 +0200

    Design of dashboard and navigation bar

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Timers;
+using System.Windows;
+
+namespace Stock_Management
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Ticker : INotifyPropertyChanged
+    {
+        public Ticker()
+        {
+            System.Timers.Timer timer = new System.Timers.Timer
+            {
+                Interval = 1000 // 1 second updates
+            };
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+        }
+
+        public DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Now)));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+    }
+}
