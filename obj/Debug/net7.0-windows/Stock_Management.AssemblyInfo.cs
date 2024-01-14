477c97169ee6dbe61db83524e1cf184ee770864d
luyandohambala <luyandohambala.1@gmail.com>
2024-01-15 01:57:21 +0200

    product table added and data manipulation buttons added, font colors changed

--- obj/Debug/net7.0-windows/Stock_Management.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/Stock_Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stock_Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+137620562d0d9bec327727c29aef0687c561e43c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb44bbbdd3cb2addf3d625c43740127de80834a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stock_Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stock_Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
